15ef775e6af48ad051cfe75b4b0ee46c19e7f433
Nishashetty664 <144149709+Nishashetty664@users.noreply.github.com>
2024-07-10 21:07:18 +0530

    Delete generate_requirements.py

--- generate_requirements.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def freeze_requirements():
-    with open('requirements.txt', 'w') as f:
-        result = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE, text=True)
-        f.write(result.stdout)
-
-if __name__ == "__main__":
-    freeze_requirements()
